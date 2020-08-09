9ccc36f30c0f77822f47906345caad6ce3b064a2
Aaron Marshall <hi@imaaronnicetomeetyou.me>
2020-08-09 22:39:51 +1000

    Adds client deployment

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,10 +1,19 @@
+using System.Text.Json.Serialization;
+
 namespace ServerlessTodo.Models
 {
 	public class Todo 
 	{
+		[JsonPropertyName("summary")]
 		public string Summary { get; set; }
+
+		[JsonPropertyName("description")]
 		public string Description { get; set; }
+
+		[JsonPropertyName("tags")]
 		public string[] Tags { get; set; }
+
+		[JsonPropertyName("isComplete")]
 		public bool IsComplete { get; set; }
 	}
 }
